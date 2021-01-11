6de45104dbd042ba3cffa11dc2f2f25555d7482d
jorbuedo_nexum <jordi.bueno@nexum.com>
2021-01-11 12:10:32 +0100

    [ci skip] Updates

---
+++ precache-manifest.a5977f5104c0ac11fbd11eecd7ce62fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf89f7f0b735edbf4f72b51a2a124ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1235cdd1e0b98542dbf",
+    "url": "./static/js/2.516e1d2c.chunk.js"
+  },
+  {
+    "revision": "2e4ec4ef8914db3556357fa782e7ec80",
+    "url": "./static/js/2.516e1d2c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2117e32a2b1e795d29f8",
+    "url": "./static/js/main.0558d595.chunk.js"
+  },
+  {
+    "revision": "e53eb449e83d57b8f5cd",
+    "url": "./static/js/runtime-main.ba520f22.js"
+  }
+]);
